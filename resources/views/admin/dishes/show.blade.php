60de3b3379f26a0d9028fe646d8fec3cf2b86361
NicolaBellini <bellininicola4366@gmail.com>
2024-06-17 17:38:10 +0200

    [add] show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -10,13 +10,22 @@
         <div class="mt-4">
             <img src="{{asset('storage/'.$dish->image)}}" alt="">
         </div>
+        @else
+         <div class="mt-4">
+            <img src="{{asset('storage/img/placeholder.png')}}" alt="">
+        </div>
         @endif
         <div>
-            <p>Description</p>
+            <p>{{$dish->description}}</p>
         </div>
 
         <div>
-            <p>Price</p>
+            <p>{{$dish->price}} &euro;</p>
+        </div>
+
+        <div>
+           <p>Is available: {{ $dish->is_visible ? 'È visibile' : 'Non è disponibile' }}</p>
+
         </div>
     </div>
 @endsection
