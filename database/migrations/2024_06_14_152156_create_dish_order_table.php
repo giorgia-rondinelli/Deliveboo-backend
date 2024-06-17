48c6d96a23b698006442dcd92bce0f7df7c14742
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-06-17 10:15:07 +0200

    [add] migration dish-order

---
+++ database/migrations/2024_06_14_152156_create_dish_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('dish_id');
+            $table->foreign('dish_id')
+                  ->references('id')
+                  ->on('dishes')
+                  ->cascadeOnDelete();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')
+                  ->references('id')
+                  ->on('orders')
+                  ->cascadeOnDelete();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dish_order');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            TypesTableSeeder::class
+            TypesTableSeeder::class,
+            DishesTableSeeder::class,
+            RestaurantTableSeeder::class,
+
         ]);
     }
 }
